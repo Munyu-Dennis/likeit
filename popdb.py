a56cfaa5f5427eb89b27fd061d2e744fad65c2b2
Dennis Munyu <ituromunyu@gmail.com>
2020-09-15 15:08:12 +0300

    deleted some files

--- popdb.py
+++
@@ -1,8 +0,0 @@
-import json
-from home.models import Post
-with open('posts.json') as f:
-    posts = json.load(f)
-
-for post in posts:
-    post = Post(author=post['author'], link=post['link'], title=post['title'], content=post['content'], slug=post['slug'])
-    post.save()
